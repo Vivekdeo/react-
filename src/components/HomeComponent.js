07803a632f27312991cf054ee9a0b07a893276f4
mario056 <vivek1404pratap@gmail.com>
2019-03-25 15:28:15 +0530

    Single page application part1

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import { Card ,CardBody,CardImg,CardTitle,CardSubtitle,CardText} from 'reactstrap';
+import { from } from 'rxjs';
 
+function Rendercard({item}){
+    return(
+        <Card>
+            <CardImg src={item.src} alt={item.name}></CardImg>
+            <CardBody>
+            <CardTitle >{item.name}</CardTitle>
+            {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
+            <CardText>
+                {item.description}
+            </CardText>
+            </CardBody>
+        </Card>
+    );
+}
 
 function Home(props){
     return(
         <div className="container">
-        <h1>Home</h1>
+            <div className="row align-items-start">
+                <div className="col-12 col-md m-1">
+                <Rendercard item={props.dish}></Rendercard>
+                </div>
+                <div className="col-12 col-md m-1">
+                <Rendercard item={props.promotion}></Rendercard>
+                </div>
+                <div className="col-12 col-md m-1">
+                <Rendercard item={props.leader}></Rendercard>
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/MainComponents.js
+++ src/components/MainComponents.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import Menu from './MenuComponents';
 import DishDetails from './dishdetails'
 import { Navbar, NavbarBrand } from 'reactstrap';
-import {DISHES} from '../shared/dishes';
 import Header from './Header';
 import Footer from './Footer';
 import Home from './HomeComponent';
+import Contact from './ContactComponents';
 import {Switch,Route,Redirect} from 'react-router-dom';
+import {DISHES} from '../shared/dishes';
+import {COMMENTS} from '../shared/comments';
+import {PROMOTIONS} from '../shared/promotions';
+import {LEADERS} from '../shared/leaders';
 
 
 class Main extends Component {
@@ -14,7 +18,9 @@ class Main extends Component {
     super(props);
     this.state={
       dishes: DISHES,
-      
+      comments:COMMENTS,
+      promotions:PROMOTIONS,
+      leaders:LEADERS
     };
   }
 
@@ -23,7 +29,11 @@ class Main extends Component {
   render() {
     const HomePage=()=>{
       return(
-        <Home/>
+        <Home dish={this.state.dishes.filter((dish)=>dish.featured)[0]}
+        promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          
+        />
       );
     }
     return (
@@ -32,6 +42,7 @@ class Main extends Component {
           <Switch>
             <Route path="/home" component={HomePage}></Route>
             <Route exact path="/menu" component={()=> <Menu dishes={this.state.dishes}></Menu>}></Route>
+            <Route exact path="/contactus" component={Contact}></Route>
             <Redirect to="/home"></Redirect>
           </Switch>
           <Footer/>
